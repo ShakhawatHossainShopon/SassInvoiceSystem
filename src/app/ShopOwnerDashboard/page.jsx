40fa1ae21de98d70f6ffb308d49a8adf2cc4b034
ShakhawatHossainShopon <shoponhossain32186@gmail.com>
2025-03-11 14:03:51 +0600

    Initialize Project

---
+++ src/app/ShopOwnerDashboard/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>shop owner</div>;
+};
+
+export default page;
---
+++ src/app/api/admin/auth/register/route.js
@@ -0,0 +1,26 @@
+import { connectToDB } from "@/lib/mongodb";
+import Admin from "@/app/models/Admin";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const { email, password, username } = await req.json();
+    await connectToDB();
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return NextResponse.json(
+        { error: "Admin Already Exist" },
+        { status: 400 }
+      );
+    }
+    // password hash
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const NewAdmin = new Admin({ email, username, password: hashPassword });
+    await NewAdmin.save();
+    return NextResponse.json({ error: "Admin Created Succesfully" });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to Register" }, { status: 500 });
+  }
+}
