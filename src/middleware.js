a8c98c51542406b83f39aadd69aec190cbc9fa5a
ShakhawatHossainShopon <shoponhossain32186@gmail.com>
2025-03-16 22:47:05 +0600

    bug fixed

--- src/middleware.js
+++ src/middleware.js
@@ -4,26 +4,18 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const token = request.cookies.get("Admintoken");
-    // Set CORS headers for all API requests
-  const res = NextResponse.next();
-  res.headers.set("Access-Control-Allow-Origin", "*"); // Replace with your frontend domain for security
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // Handle OPTIONS (preflight) request for CORS
-  if (request.method === "OPTIONS") {
-    return new Response(null, { status: 204 });
-  }
+
 
   if (!token) {
     // No userToken, redirect to homepage
     return NextResponse.redirect(new URL("/Admin", request.nextUrl));
   }
 
-   return res;
+  return NextResponse.next();
 }
 
 // Apply middleware to routes under '/CutomerDashboard/*' and '/Admin/*'
 export const config = {
-  matcher: ["/Admin/(.*)", "/api/:path*"],
+  matcher: ["/Admin/(.*)"],
 };
